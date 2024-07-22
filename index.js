c444a6b961a1b614373ac434198599f727ef0275
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-07-22 14:10:47 -0700

    app complete staged for testing and debugging

--- index.js
+++ index.js
@@ -18,10 +18,30 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+
+// No need for bearer token or configuration variables etc. 
+
+app.use(express.static("public")); 
+
+app.get("/", async (req, res) => {
+    try{
+        const result = await axios.get(API_URL + "/random"); 
+        res.render("index.ejs", {
+        secret: result.data.secret,
+        user: result.data.username,
+    });
+    } catch(error){
+        console.log(error.response.data);
+        res.status(500); 
+    }
+});
 
 
 
 
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
